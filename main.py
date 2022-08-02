b5264ce31a8eaa06609ab9c0dfa0dbd42e3c10f7
sent1nu11 <sent1nu11@gmail.com>
2022-08-01 22:11:43 -0600

    incorporate lists to the destination path

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import os, shutil
 
-source_path = '/mnt/ext4_data/Music/Anastacia/Freak of Nature/Anastacia -  Paid My Dues.mp3'
+source_file = '/mnt/ext4_data/Music/Anastacia/Freak of Nature/Anastacia -  Paid My Dues.mp3'
 dest_path = '/media/rogerwu/MUSIC'
+slash = '/'
 
-a_file = open("Energy_Playlist.m3u", "r")
-list_of_lists = []
-# for line in a_file:
-# #    stripped_line = line.strip()
-# #    line_list = stripped_line.split()
-#     if line[0] == '/':
-#         list_of_lists.append(line.strip())
-#
-# a_file.close()
+playlist_file = open("Energy_Playlist.m3u", "r")
+playlist_lists = []
+for line in playlist_file:
+#    stripped_line = line.strip()
+#    line_list = stripped_line.split()
+    if line[0] == '/':
+        playlist_lists.append(line.strip())
+
+playlist_file.close()
+
+for filepath in playlist_lists:
+    source_filepath = filepath
+    destination_folderpath= source_filepath.rfind('/')+1   # ie /media/rogerwu/MUSIC
+    print(f"destination_folderpath: {destination_folderpath}")
+    last_slash_position = source_filepath.rfind('/')+1
+    print(f"last_slash_position: {last_slash_position}")
+    music_file = source_filepath[last_slash_position:0]
+    print(f"source_filepath : {source_filepath}")
+    print(f"music_file : {music_file}")
+    destination_filepath = destination_folderpath + slash + music_file
+    print(destination_filepath)
+
+# print(playlist_lists)
 # #
-# for i in list_of_lists:
-#     if i[0] == '#':
-#         del i[0]
+# lastslash_position = source_file.rfind('/')+1
+# print(lastslash_position)
+# music_file = source_file[lastslash_position:-1]
+# print("Music_file", music_file)
 
-# print(list_of_lists)
+# dest_file = dest_path + "/" + file
 
-slash = source_path.rfind('/')+1
-print(slash)
-file = source_path[slash:-1]
-print(file)
-
-dest_file = dest_path + '/' + file
-
-shutil.copy(source_path, dest_file)
+# shutil.copy(source_path, dest_file)
